71aab93b04265a4d097002a5e164d873fb5b7ccd
CSGrandeur <csgrandeur@gmail.com>
2014-07-17 22:45:47 +0800

    both print client and server

---
+++ PrinterManager/PrinterManager/SendThread.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrinterManager
+{
+    class SendThread
+    {
+        public SendThread() { }
+        public List<PrintSystemJobInfo> joblist = new List<PrintSystemJobInfo>();
+        public void SendInfo()
+        {
+            InfoToSend its = new InfoToSend();
+            string sendstr = "";
+            //using (System.IO.StreamWriter sw = new System.IO.StreamWriter("D:/PrinterManagerLog.txt", true))
+            //{
+            //    sw.WriteLine(joblist.Count + "\t\r\n");
+            //}
+            if (joblist.Count > 0)
+            {
+                for (int i = 0; i < joblist.Count; i++)
+                {
+                    its.submitter = joblist[i].Submitter;
+                    its.pagenum = joblist[i].NumberOfPages;
+                    its.jobname = joblist[i].JobName;
+                    its.identifier = joblist[i].JobIdentifier;
+                    its.jobtime = joblist[i].TimeJobSubmitted + "";
+                    sendstr = its.ToHttpGetStr();
+                }
+            }
+        }
+    }
+}
