a334ed38a3a87923a7ffe3b0d722da4471967d87
CSGrandeur <CSGrandeur@gmail.com>
2014-08-15 19:22:37 +0800

    ！

--- PrinterManager/PrinterManager/bin/Debug/Install.bat
+++ PrinterManager/PrinterManager/bin/Debug/Install.bat
@@ -1,5 +1,5 @@
-D:
-cd D:/workspace/LabManageSystem/PrinterManager/PrinterManager/bin/Debug
+E:
+cd E:/workspace/LabManageSystem/PrinterManager/PrinterManager/bin/Debug
 %SystemRoot%/Microsoft.NET/Framework/v4.0.30319/installutil.exe PrinterManager.exe
 sc config PrinterManager start= auto
 Net Start PrinterManager
--- PrinterManager/PrinterManager/bin/Debug/Uninstall.bat
+++ PrinterManager/PrinterManager/bin/Debug/Uninstall.bat
@@ -1,3 +1,3 @@
-D:
-cd D:/workspace/LabManageSystem/PrinterManager/PrinterManager/bin/Debug
+E:
+cd E:/workspace/LabManageSystem/PrinterManager/PrinterManager/bin/Debug
 %SystemRoot%/Microsoft.NET/Framework/v4.0.30319/installutil.exe /u PrinterManager.exe
