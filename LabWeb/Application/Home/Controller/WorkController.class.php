8a571a607e8654e47c8b385937a7bceb9b032746
CSGrandeur <csgrandeur@gmail.com>
2014-09-01 19:15:12 +0800

    ~

--- LabWeb/Application/Home/Controller/WorkController.class.php
+++ LabWeb/Application/Home/Controller/WorkController.class.php
@@ -367,6 +367,7 @@ class WorkController extends Controller {
 									LIMIT ".$d_start.",".$d_length."
 									");
 		file_put_contents("/var/www/loog.txt", print_r($reqdata, true)."\n", FILE_APPEND);
+		file_put_contents("/var/www/loog.txt", print_r($User->_sql(), true)."\n", FILE_APPEND);
 			if($userlist != null && $userlist[0]['uid'] != null)
 			{
 				for($i = count($userlist) - 1; $i >= 0; $i --)
