77b428bbeccd4593fb21add1030fa883a48b01c5
CSGrandeur <csgrandeur@gmail.com>
2014-09-18 13:40:48 +0800

    ~~

--- LabWeb/Application/Common/Common/function.php
+++ LabWeb/Application/Common/Common/function.php
@@ -155,7 +155,7 @@ function char2utf8($data){
 	{
 		$fileType = mb_detect_encoding($data , array('UTF8','GBK','LATIN1','BIG5')) ;
 		if( $fileType != 'UTF-8'){
-			$data = mb_convert_encoding($data ,'utf8' , $fileType);
+			$data = mb_convert_encoding($data ,'UTF-8' , "auto");
 		}
 	}
 	return $data;
